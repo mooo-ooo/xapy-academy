3283207dadd678943305064bbc49d03d1324dd4e
Duy Huynh <huquduy@gmail.com>
2026-06-03 21:52:07 +0700

    feat: build

--- .next/dev/server/middleware.js
+++
@@ -1,9 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("server/middleware.js")
-R.c("server/chunks/node_modules_next_05jgm7v._.js")
-R.c("server/chunks/node_modules_@auth_core_0sqr-v1._.js")
-R.c("server/chunks/node_modules_jose_dist_webapi_0iwu60c._.js")
-R.c("server/chunks/node_modules_zod_v4_0jjwlr5._.js")
-R.c("server/chunks/[root-of-the-server]__0e6vr1f._.js")
-R.c("server/chunks/[root-of-the-server]__01_sq9e._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/src/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/src/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)").exports
